21ea39a9fb8bf033571309a2977a7d1335681a77
Eric Lewis <ericlewis777@gmail.com>
2021-06-20 11:10:21 -0400

    feat: v1 of recursive comment fetching

--- src/build.js
+++ src/build.js
@@ -4,35 +4,14 @@
 const fastify = require("fastify");
 const { decode } = require("html-entities");
 const fetch = require("undici-fetch");
-const firebase = require("firebase");
-const any = require("promise.any");
-
 const { NodeHtmlMarkdown } = require("node-html-markdown");
 
 const nhm = new NodeHtmlMarkdown();
-
-const firebaseConfig = {
-  databaseURL: "https://hacker-news.firebaseio.com/"
-};
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
 
 async function fetchItemURL(id) {
   const url = `https://hacker-news.firebaseio.com/v0/item/${id}`;
   const response = await fetch(url);
   return response.json();
-}
-
-async function fetchItemFirebase(id) {
-  const response = await database
-    .ref(`v0/item/${id.replace(".json", "")}`)
-    .get();
-  return response.val();
-}
-
-function fetchItem(id) {
-  return any([fetchItemFirebase(id), fetchItemURL(id)]);
 }
 
 async function fetchIdsURL(endpoint) {
@@ -41,22 +20,41 @@ async function fetchIdsURL(endpoint) {
   return response.json();
 }
 
-async function fetchIdsFirebase(endpoint) {
-  const response = await database
-    .ref(`v0/${endpoint.replace(".json", "")}`)
-    .get();
-  return response.val();
+function parseText(item) {
+  if (item.text) {
+    return nhm.translate(decode(item.text));
+  }
+
+  return undefined;
 }
 
-function fetchIds(endpoint) {
-  return any([fetchIdsURL(endpoint), fetchIdsFirebase(endpoint)]);
+async function recursiveComments(item) {
+  let kids;
+  if (item.kids) {
+    kids = await Promise.all(
+      item.kids.map(async (id) => {
+        const item = await fetchItemURL(`${id}.json`);
+        const kids = await recursiveComments(item);
+
+        const text = parseText(item);
+
+        return {
+          ...item,
+          kids,
+          text
+        };
+      })
+    );
+  }
+
+  return kids;
 }
 
 function build(opts) {
   const app = fastify(opts);
 
   app.get("/v0/item/:id", async (req, _reply) => {
-    const item = await fetchItem(req.params.id);
+    const item = await fetchItemURL(req.params.id);
 
     if (item.id === undefined || item.id === null) {
       return;
@@ -73,9 +71,28 @@ function build(opts) {
     return result;
   });
 
+  app.get("/v1/item/:id", async (req, _reply) => {
+    const item = await fetchItemURL(req.params.id);
+    const text = parseText(item);
+
+    const kids = await recursiveComments(item);
+
+    const result = {
+      ...item,
+      kids,
+      text
+    };
+
+    return result;
+  });
+
   app.get("/v0/:endpoint", async (req, _reply) => {
-    const ids = await fetchIds(req.params.endpoint);
-    return Promise.all(ids.map((id) => fetchItem(`${id}.json`)));
+    const ids = await fetchIdsURL(req.params.endpoint);
+    return Promise.all(ids.map((id) => fetchItemURL(`${id}.json`)));
+  });
+
+  app.get("/", (_req, _reply) => {
+    return { message: "whatchu lookin at willis?" };
   });
 
   return app;
